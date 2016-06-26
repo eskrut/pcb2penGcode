4a96ddfb5866a1a08d8e748cb88cf006ce10e51b
Evgeny Krutko <e.s.krutko@gmail.com>
2016-06-26 22:49:13 +0300

    process svg files directly

--- svg2penGcode.py
+++ svg2penGcode.py
@@ -3,14 +3,10 @@
 import sys, os
 import getopt
 import ConfigParser
-import re
-from svg.path import Path, Line, Arc, CubicBezier, QuadraticBezier, parse_path
-from xml.dom import minidom
 import numpy as np
 import math
-from shapely.geometry import LinearRing
 from pprint import pprint
-
+from svgProcessor import *
 
 def formatVal(val, f="%0.2f"):
     return (f % (val))
@@ -18,74 +14,6 @@ def cmp_start_node(path1, path2):
     x1, y1 = path1.xy
     x2, y2 = path2.xy
     return x1[0] < x2[0]
-def generateInfil(lineRing, penRadius, minPenRadius, maxIterations, side='left', join_style=2):
-    if penRadius < minPenRadius:
-        penRadius = minPenRadius
-    if maxIterations == 0:
-        return []
-    try:
-        offset = lineRing.parallel_offset(penRadius, side, join_style=join_style, mitre_limit=10,resolution=16)
-    except:
-        return []
-    parts = hasattr(offset, 'geoms') and offset or [offset]
-    parts = sorted(parts, cmp=cmp_start_node)
-    # Check if infil generated in right side. If not bounds of infil will be wider than bounds of original
-    minX = minY = 1e10
-    maxX = maxY = -1e10
-    for p in parts:
-        try:
-            x, y = p.xy
-            minX = min(minX, min(x))
-            minY = min(minY, min(y))
-            maxX = max(maxX, max(x))
-            maxY = max(maxY, max(y))
-        except:
-            # print(p)
-            pass
-    try:
-        ox, oy = lineRing.xy
-        if minX < min(ox) or minY < min(oy) or maxX > max(ox) or maxY > max(oy):
-            # Make with right side
-            try:
-                offset = lineRing.parallel_offset(penRadius, 'right', join_style=join_style, mitre_limit=10,resolution=16)
-            except:
-                pass
-            parts = hasattr(offset, 'geoms') and offset or [offset]
-    except:
-        pass
-    # for i in range(len(parts)):
-    #     try:
-    #         x,y = parts[i].xy
-    #         nodes = zip(x, y)
-    #         parts[i] = LinearRing(nodes)
-    #     except:
-    #         pass
-    subparts = []
-    for p in parts:
-        p = p.simplify(0.05)#(penRadius/4)
-        # if len(p.coords) > 2:
-        #p = LinearRing(p)
-        subparts.append(p)
-        subparts += generateInfil(p, penRadius/2.5, minPenRadius, maxIterations=maxIterations-1, side='left',join_style=join_style)
-    return subparts
-def generatePathes(borderNodes, penRadius, minPenRadius, maxIterations=2, includeBorder=False, insideFirst=False, jointStyle=2):
-    l = LinearRing(borderNodes)
-    offsets = generateInfil(l, penRadius, minPenRadius, maxIterations,join_style=jointStyle)
-    if len(offsets) == 0:
-        raise RuntimeError("No infil generated (")
-    if includeBorder:
-        paths = [l] + offsets
-    else:
-        paths = offsets
-    if insideFirst:
-        return list(reversed(paths))
-    else:
-        return paths
-def pathLength(path):
-    length = 0
-    for ct in range(len(path)-1):
-        length += math.sqrt( (path[ct+1][0] -  path[ct][0])**2.0 + (path[ct+1][1] -  path[ct][1])**2.0 )
-    return length
 def genGCode(path, zDraw, zMove, xOffset, yOffset, zOffset, drawSpeed, moveSpeed, zMoveSpeed):
     code = []
     #code += 'G1 Z'+formatVal(zMove + zOffset)+';\n'
@@ -104,8 +32,6 @@ def printProgress (iteration, total, prefix = '', suffix = '', decimals = 2, bar
     sys.stdout.flush()
     if iteration == total:
         print("\n")
-def getDistance(node1, node2):
-    return math.sqrt((node1[0] - node2[0])**2 + (node1[1] - node2[1])**2)
 
 
 def main(argv):
@@ -229,152 +155,18 @@ def main(argv):
         elif opt in ("--mirrored"):
             mirrored = True
     
-    correctedPaths = []
-    for fileName in args:
-        doc = minidom.parse(fileName)
-        pathstrings = [path.getAttribute('d') for path in doc.getElementsByTagName('path')]
-
-        r = re.compile('M([-+]?[0-9]*\.?[0-9]*)\s([-+]?[0-9]*\.?[0-9]*)\s')
-        tmp = pathstrings
-        pathstrings = []
-        for path in tmp:
-            pos = path.find('z m')
-            if pos > 0:
-                paths = path.split('z m')
-                paths[0] += 'z'
-                sb = r.search(paths[0])
-                base = [float(sb.group(1)), float(sb.group(2))]
-                for i in range(1, len(paths)):
-                    paths[i] = 'M' + paths[i]
-                    sr = r.search(paths[i])
-                    rel = [float(sr.group(1)), float(sr.group(2))]
-                    rr = re.compile('M([-+]?[0-9]*\.?[0-9]*\s[-+]?[0-9]*\.?[0-9]*)')
-                    ss = rr.search(paths[i])
-                    paths[i] = paths[i].replace(ss.group(1), str(base[0]+rel[0]) + ' ' + str(base[1]+rel[1]))
-                    paths[i] = paths[i] + 'z'
-                    base = [base[0]+rel[0], base[1]+rel[1]]
-                pathstrings.append(paths)
-            else:
-                pathstrings.append([path])
-
-        transform = doc.getElementsByTagName('g')[0].getAttribute('transform')
-        rt = re.compile('translate\(([-+]?[0-9]*\.?[0-9]*,[-+]?[0-9]*\.?[0-9]*)\)')
-        rs = re.compile('scale\(([-+]?[0-9]*\.?[0-9]*,[-+]?[0-9]*\.?[0-9]*)\)')
-        st = rt.search(transform)
-        ss = rs.search(transform)
-
-        translate = [float(val) for val in st.group(1).split(',')]
-        scale     = [float(val) for val in ss.group(1).split(',')]
-
-
-        r = re.compile('([mMcClL]?[-+]?[0-9]*\.?[0-9]*\s[-+]?[0-9]*\.?[0-9]*[z\s^]?)')
-        rs = re.compile('([-+]?[0-9]*\.?[0-9]*)[\s,]([-+]?[0-9]*\.?[0-9]*?)')
-        rsf = re.compile('([-+]?[0-9]*\.?[0-9]*[\s,][-+]?[0-9]*\.?[0-9]*?)')
-
-        for paths in pathstrings:
-            correctedPaths.append([])
-            for path in paths:
-                descriptions = r.findall(path)
-                for i in range(len(descriptions)):
-                    s = rs.search(descriptions[i])
-                    sf = rsf.search(descriptions[i])
-                    vals = [float(val) for val in [s.group(1), s.group(2)] ]
-                    vals[0] *= scale[0]
-                    vals[1] *= -scale[1]
-                    if i == 0:
-                        vals[0] += translate[0]
-                        vals[1] += translate[1]
-                    vals[0] *= userScale
-                    vals[1] *= userScale
-                    if i == 0:
-                        vals[0] += xOffset
-                        vals[1] += yOffset
-                    descriptions[i] = descriptions[i].replace(sf.group(1), str(vals[0]) + ' ' + str(vals[1]))
-                correctedPaths[-1].append(''.join(descriptions))
-
-    paths = []
-    pathsNodes = []
-    minX = 1e10
-    minY = 1e10
-    maxX = -1e10
-    maxY = -1e10
-    count = 0
-    for cPaths in correctedPaths:
-        printProgress(count, len(correctedPaths), prefix='Parsing')
-        paths.append([])
-        pathsNodes.append([])
-        for c in cPaths:
-            p = parse_path(c)
-            paths[-1].append(p)
-            pathsNodes[-1].append([])
-            # for subPath in paths[-1]:
-            aggL = 0
-            length = p.length()
-            numCheck = int(length/penRadius*9.9)
-            if length > 1:
-                checkPoints = [p.point(cl) for cl in np.arange(0, 0.999, 1.0/numCheck)]
-                minX = min([minX, min([p.real for p in checkPoints ])])
-                minY = min([minY, min([p.imag for p in checkPoints ])])
-                maxX = max([maxX, max([p.real for p in checkPoints ])])
-                maxY = max([maxY, max([p.imag for p in checkPoints ])])
-                index = 1
-                while index < len(checkPoints):
-                    aggL += abs(checkPoints[index] - checkPoints[index-1])
-                    if aggL > penRadius or aggL == 0:
-                        pathsNodes[-1][-1].append([checkPoints[index].real, checkPoints[index].imag])
-                        aggL = 0
-                    index += 1
-        count += 1
-    printProgress(1, 1, prefix='Parsing', suffix='Done')
-    tmp = pathsNodes
-    pathsNodes = []
-    for pp in tmp:
-        pathsNodes.append([])
-        for p in pp:
-            pathsNodes[-1].append([])
-            for n in p:
-                x = n[0]
-                y = n[1]
-                if mirrored:
-                    x = minX + maxX - x
-                    pathsNodes[-1][-1].append([y - minY, maxX - x])
-                else:
-                    pathsNodes[-1][-1].append([x - minX, y - minY])
-    if mirrored:
-        t = maxY
-        maxY = maxX
-        maxX = t
-        t = minY
-        minY = minX
-        minX = t
+    fileName = args[0]
+    parcer = SVGProcessor()
+    parcer.parce(fileName, penRadius)
+    parcer.process(mirrored)
     code = []
     code.append('G21;\nG90;\nG28 X0 Y0;\nG28 Z0;\n')
     code.append('G1 Z' + formatVal(zMove + zOffset + zApproach) + ';\n')
     count = 0
-    lastNode = None
-    for nodesGroup in pathsNodes:
-        printProgress(count, len(pathsNodes), prefix='Generating')
-        if len(nodesGroup) > 1:
-            maxIter = 1
-        else:
-            maxIter = maxInfilIterations
-        for nodes in nodesGroup:
-            if len(nodes) == 0:
-                continue
-            offsets = generatePathes(nodes, penRadius, maxIterations=maxIter, includeBorder=includeBorder, minPenRadius=minPenRadius, insideFirst=insideFirst, jointStyle=jointStyle)
-            if len(offsets) > numRepeatFirst:
-                offsets = [offsets[:numRepeatFirst]] + offsets
-            else:
-                offsets = offsets + offsets
-            for offset in offsets:
-                try:
-                    x,y = offset.xy
-                    nodes = zip(x, y)
-                    if pathLength(nodes) > 1:
-                        code += genGCode(nodes, zDraw, zMove, xOffset, yOffset, zOffset, feed, moveFeed, zMoveFeed)
-                except:
-                    # print offset
-                    pass
+    allPaths = parcer.penPaths()
+    for penPath in allPaths:
+        printProgress(count, len(allPaths), prefix='Generating')
+        code += genGCode(penPath, zDraw, zMove, xOffset, yOffset, zOffset, feed, moveFeed, zMoveFeed)
         count += 1
     code.append('G1 Z' + formatVal(zMove + zOffset + zRetreat) + ';\n')
     code.append('G28 X0;\n')
@@ -383,11 +175,12 @@ def main(argv):
     f.writelines(code)
     f.close()
     printProgress(1, 1, prefix='Generating', suffix='Done')
-    print 'PCB size ', formatVal(maxX - minX, f="%0.f"), formatVal(maxY - minY, f="%0.f")
+    width, height = parcer.PCBSize()
+    print 'PCB size ', formatVal(width, f="%0.f"), formatVal(height, f="%0.f")
 
     if makeSetupFile:
         #Gen setup code for pointing to corners
-        targets = {'LB':[xOffset, yOffset], 'LT':[xOffset, yOffset+(maxY-minY)], 'RT':[xOffset+(maxX-minX), yOffset+(maxY-minY)], 'RB':[xOffset+(maxX-minX), yOffset]}
+        targets = {'LB':[xOffset, yOffset], 'LT':[xOffset, yOffset+(height)], 'RT':[xOffset+(width), yOffset+(height)], 'RB':[xOffset+(width), yOffset]}
         for k in targets:
             code = []
             code.append('G21;\nG90;\nG28 X0 Y0;\nG28 Z0;\n')
@@ -404,11 +197,11 @@ def main(argv):
         cornerNodes.append([])
         cornerNodes[-1].append([[0.0, 0.0 + stroke], [0.0, 0.0], [0.0 + stroke, 0.0]])
         cornerNodes.append([])
-        cornerNodes[-1].append([[(maxX-minX) - stroke, 0.0], [(maxX-minX), 0.0], [(maxX-minX), 0.0 + stroke]])
+        cornerNodes[-1].append([[(width) - stroke, 0.0], [(width), 0.0], [(width), 0.0 + stroke]])
         cornerNodes.append([])
-        cornerNodes[-1].append([[(maxX-minX), (maxY-minY) - stroke], [(maxX-minX), (maxY-minY)], [(maxX-minX) - stroke, (maxY-minY)]])
+        cornerNodes[-1].append([[(width), (height) - stroke], [(width), (height)], [(width) - stroke, (height)]])
         cornerNodes.append([])
-        cornerNodes[-1].append([[0.0 + stroke, (maxY-minY)], [0.0, (maxY-minY)], [0.0, (maxY-minY) - stroke]])
+        cornerNodes[-1].append([[0.0 + stroke, (height)], [0.0, (height)], [0.0, (height) - stroke]])
         code = []
         code.append('G21;\nG90;\nG28 X0 Y0;\nG28 Z0;\n')
         code.append('G1 Z' + formatVal(zMove + zOffset + zApproach) + ';\n')
@@ -420,29 +213,6 @@ def main(argv):
         code.append('G28 X0;\n')
         code.append('M84;\n;')
         f = open(fileName+'.corners.gcode', 'w')
-        f.writelines(code)
-        f.close()
-        #Gen code for draw borders
-        code = []
-        code.append('G21;\nG90;\nG28 X0 Y0;\nG28 Z0;\n')
-        code.append('G1 Z' + formatVal(zMove + zOffset + zApproach) + ';\n')
-        for nodesGroup in [pathsNodes[0]]:
-            maxIter = 1
-            for nodes in nodesGroup:
-                offsets = generatePathes(nodes, penRadius, maxIterations=maxIter, includeBorder=includeBorder, minPenRadius=minPenRadius)
-                offsets = [offsets[0]] + offsets
-                for offset in offsets:
-                    try:
-                        x,y = offset.xy
-                        nodes = zip(x, y)
-                        code += genGCode(nodes, zDraw, zMove, xOffset, yOffset, zOffset, feed, moveFeed, zMoveFeed)
-                    except:
-                        pass
-                        #print offset
-        code.append('G1 Z' + formatVal(zMove + zOffset + zRetreat) + ';\n')
-        code.append('G28 X0;\n')
-        code.append('M84;\n;')
-        f = open(fileName+'.border.gcode', 'w')
         f.writelines(code)
         f.close()
 
---
+++ svgProcessor.py
@@ -0,0 +1,248 @@
+from xml.dom import minidom
+import numpy as np
+import re
+from svg.path import Path, Line, Arc, CubicBezier, QuadraticBezier, parse_path
+from shapely.geometry import LineString
+from shapely.geometry import LinearRing
+
+def cmp_start_node(path1, path2):
+    x1, y1 = path1.xy
+    x2, y2 = path2.xy
+    return x1[0] < x2[0]
+def generateInfil(lineRing, penRadius, side='right', join_style=2):
+    try:
+        offset = lineRing.parallel_offset(penRadius, side, join_style=join_style, mitre_limit=10,resolution=16)
+    except:
+        return []
+    parts = hasattr(offset, 'geoms') and offset or [offset]
+    parts = sorted(parts, cmp=cmp_start_node)
+    # Check if infil generated in right side. If not bounds of infil will be wider than bounds of original
+    minX = minY = 1e10
+    maxX = maxY = -1e10
+    for p in parts:
+        try:
+            x, y = p.xy
+            minX = min(minX, min(x))
+            minY = min(minY, min(y))
+            maxX = max(maxX, max(x))
+            maxY = max(maxY, max(y))
+        except:
+            pass
+    try:
+        ox, oy = lineRing.xy
+        if minX < min(ox) or minY < min(oy) or maxX > max(ox) or maxY > max(oy):
+            # Make with 'right' side
+            try:
+                offset = lineRing.parallel_offset(penRadius, 'left', join_style=join_style, mitre_limit=10,resolution=16)
+            except:
+                pass
+            parts = hasattr(offset, 'geoms') and offset or [offset]
+    except:
+        pass
+    subparts = []
+    for p in parts:
+        p = p.simplify(0.05)
+        if len(p.coords)  > 2:
+            subparts.append(p)
+            subparts += generateInfil(p, penRadius, side='right',join_style=join_style)
+    return subparts
+class PCBEntry(object):
+    """docstring for PCBEntry"""
+    def __init__(self):
+        super(PCBEntry, self).__init__()
+        self.penPaths = []
+    def process(self):
+        raise NotImplemented("virtual class")
+    def minMax(self):
+        if len(self.penPaths) == 0:
+            return 1e10, -1e10, 1e10, -1e10
+        minX = []
+        maxX = []
+        minY = []
+        maxY = []
+        for p in self.penPaths:
+            a = np.array(p)
+            maxX.append(max(a[:,0]))
+            minX.append(min(a[:,0]))
+            maxY.append(max(a[:,1]))
+            minY.append(min(a[:,1]))
+        return min(minX), max(maxX), min(minY), max(maxY)
+    def correctCoordinates(self, minX, minY, mirrored):
+        for ct in range(len(self.penPaths)):
+            for ct2 in range(len(self.penPaths[ct])):
+                self.penPaths[ct][ct2][0] -= minX
+                self.penPaths[ct][ct2][1] -= minY
+                if mirrored:
+                    tmp = self.penPaths[ct][ct2][0]
+                    self.penPaths[ct][ct2][0] = self.penPaths[ct][ct2][1]
+                    self.penPaths[ct][ct2][1] = tmp
+
+class Track(PCBEntry):
+    """dong for AreaToFill"""
+    def __init__(self, nodes, stroke, penRadius):
+        super(Track, self).__init__()
+        self.nodes = np.array(nodes)
+        self.nodes *= np.array([1.0, -1.0])
+        self.stroke = stroke
+        self.penRadius = penRadius
+    def process(self):
+        self.penPaths.append(self.nodes)
+        line = LineString(self.nodes)
+        cumBuf = self.penRadius
+        buf = line
+        continueFlag = True
+        while continueFlag:
+            if cumBuf + self.penRadius < self.stroke/2.0:
+                step = self.penRadius
+            else:
+                step = self.stroke/2.0 - cumBuf
+                continueFlag = False
+                if step <= 0:
+                    break
+            buf = buf.buffer(step)
+            cumBuf += step
+            border = buf.exterior.simplify(0.05)
+            x, y = border.coords.xy
+            nodes = zip(x, y)
+            self.penPaths.append(np.array(nodes))
+class AreaToFill(PCBEntry):
+    """docstring for AreaToFill"""
+    def __init__(self, nodes, stroke, penRadius):
+        super(AreaToFill, self).__init__()
+        self.nodes = np.array(nodes)
+        self.nodes *= np.array([1.0, -1.0])
+        self.stroke = stroke
+        self.penRadius = penRadius
+    def process(self):
+        line = LinearRing(self.nodes)
+        infill = generateInfil(line, self.penRadius)
+        if len(infill) == 0:
+            print "failed to process infill. will try with smaller radius"
+            for c in reversed(np.arange(self.penRadius*0.5, self.penRadius*0.9, self.penRadius*0.05)):
+                infill = generateInfil(line, c)
+                if len(infill) > 0:
+                    print "Success with penRadius factor of", c/self.penRadius
+                    break
+        for part in infill: 
+            if len(part.coords) > 0:
+                x, y = part.xy
+                nodes = zip(x, y)
+                self.penPaths.append(np.array(nodes))
+        
+class SVGProcessor(object):
+    """docstring for SVGProcessor"""
+    def __init__(self):
+        super(SVGProcessor, self).__init__()
+        self.width = None
+        self.height = None
+        self.bound = None
+        self.translate = [0.0, 0.0]
+        self.scale = [1.0, 1.0]
+        self.scalefactor = 1.0
+        self.entryes = []
+    def parce(self, fileName, penRadius):
+        self.penRadius = penRadius
+        doc = minidom.parse(fileName)
+        svg = doc.getElementsByTagName("svg")[0]
+        r = re.compile("^([0-9]*\.?[0-9]*)(cm|mm)$")
+        s = r.search(svg.getAttribute("width"))
+        self.width = float(s.group(1))
+        if s.group(2) == "cm":
+            self.width *= 10;
+        s = r.search(svg.getAttribute("height"))
+        self.height = float(s.group(1))
+        if s.group(2) == "cm":
+            self.height *= 10;
+        r = re.compile("^([-+]?[0-9]*\.?[0-9]*)\s([-+]?[0-9]*\.?[0-9]*)\s([-+]?[0-9]*\.?[0-9]*)\s([-+]?[0-9]*\.?[0-9]*)\s?$")
+        s = r.search(svg.getAttribute("viewBox"))
+        self.bound = [float(s.group(1)), float(s.group(2)), float(s.group(3)), float(s.group(4))]
+        self.scalefactor = (self.width/(self.bound[2] - self.bound[0]) + self.height/(self.bound[3] - self.bound[1]))/2.0
+        groups = svg.getElementsByTagName("g")
+        rTS = re.compile("^translate\(([-+]?[0-9]*\.?[0-9]*)\s([-+]?[0-9]*\.?[0-9]*)\)\sscale\(([-+]?[0-9]*\.?[0-9]*)\s([-+]?[0-9]*\.?[0-9]*)\)$")
+        for g in groups:
+            try:
+                sTS = rTS.search(g.getAttribute("transform"))
+                self.translate = [float(sTS.group(1)), float(sTS.group(2))]
+                self.scale = [float(sTS.group(1)), float(sTS.group(2))]
+            except:
+                pass
+            style = g.getAttribute("style")
+            rFill = re.compile("fill-opacity:([0-9]*\.?[0-9]*);")
+            sFill = rFill.search(style)
+            rStroke = re.compile("stroke-opacity:([0-9]*\.?[0-9]*);")
+            sStroke = rStroke.search(style)
+            rSWidth = re.compile("stroke-width:([0-9]*\.?[0-9]*);")
+            sSWidth = rSWidth.search(style)
+            try:
+                fill = float(sFill.group(1))
+            except:
+                fill = 0.0
+            try:
+                stroke = float(sStroke.group(1))
+            except:
+                stroke = 0.0
+            try:
+                sWidth = round(float(sSWidth.group(1))*self.scalefactor, 2)
+            except:
+                sWidth = 0.0
+            paths = g.getElementsByTagName("path")
+            polylines = g.getElementsByTagName("polyline")
+            for path in paths:
+                descr = path.getAttribute("d")
+                p = parse_path(descr)
+                length = p.length()*self.scalefactor
+                numCheckPoints = length*3/penRadius
+                points = []
+                cs = [0.0]
+                for c in np.arange(1.0/numCheckPoints, 0.999, 1.0/numCheckPoints):
+                    cs.append(c)
+                cs.append(1.0)
+                for c in cs:
+                    point = p.point(c)*self.scalefactor
+                    points.append([round(point.real, 2), round(point.imag, 2)])
+                self.entryes.append(Track(points, sWidth, penRadius))
+            for polyline in polylines:
+                points = polyline.getAttribute("points")
+                nodes = []
+                for pair in points.split(" "):
+                    p = pair.split(",")
+                    if len(p) == 2:
+                        nodes.append([round(float(p[0])*self.scalefactor, 1), round(float(p[1])*self.scalefactor, 1)])
+                self.entryes.append(AreaToFill(nodes, sWidth, penRadius))
+    def process(self, mirrored):
+        minX = []
+        maxX = []
+        minY = []
+        maxY = []
+        for e in self.entryes:
+            e.process()
+            lMinX, lMaxX, lMinY, lMaxY = e.minMax()
+            minX.append(lMinX)
+            maxX.append(lMaxX)
+            minY.append(lMinY)
+            maxY.append(lMaxY)
+        self.minX = min(minX)
+        self.maxX = max(maxX)
+        self.minY = min(minY)
+        self.maxY = max(maxY)
+        for e in self.entryes:
+            e.correctCoordinates(self.minX, self.minY, mirrored)
+        if mirrored:
+            tmp = self.minX
+            self.minX = self.minY
+            self.minY = tmp
+            tmp = self.maxX
+            self.maxX = self.maxY
+            self.maxY = tmp
+    def PCBSize(self):
+        return self.maxX - self.minX + 2*self.penRadius, self.maxY - self.minY + 2*self.penRadius
+    def penPaths(self):
+        allPaths = []
+        for e in self.entryes:
+            allPaths += e.penPaths
+        return allPaths
+        
+if __name__ == "__main__":
+    parcer = SVGProcessor()
+    parcer.parce("Untitled-F.Cu.svg", 0.75/2)
+    parcer.process(False)
